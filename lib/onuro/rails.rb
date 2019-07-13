f842e9d3c57c3d91c9049d3be7ed12559be90a11
Rodrigo Reyes <rreyes@canaryhealth.com>
2019-07-13 17:37:52 -0500

    Initial commit

---
+++ lib/onuro/rails.rb
@@ -0,0 +1,8 @@
+require "onuro/rails/version"
+
+module Onuro
+  module Rails
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/onuro/rails/version.rb
@@ -0,0 +1,5 @@
+module Onuro
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
